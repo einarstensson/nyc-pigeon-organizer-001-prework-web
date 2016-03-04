f525fba2f13a0b5b9037c8dfa357676a237218da
einarstensson <einar.stensson@gmail.com>
2016-03-04 18:05:24 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  bird_names = data[:gender][:male] + data[:gender][:female]
+  new_hash = Hash.new
+
+  bird_names.each do |name|
+    new_hash[name] = {}
+
+    data.each do |key, value|
+      new_hash[name][key] = []
+
+      value.each do |attribute, bird_names|
+        new_hash[name][key] << attribute.to_s if bird_names.include?(name)
+      end
+    end
+  end
+
+  new_hash
 end
+
